c07e6ab6f1f3555a1586e8d850eddfca480ec444
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-10-03 08:12:10 -0300

    exercício

--- EX_Try_Catch/script.js
+++ EX_Try_Catch/script.js
@@ -1,13 +1,17 @@
-let raiz = document.getElementById('numero')
+let num = document.getElementById('numero')
 let resultado = document.getElementById('resultado')
 
 function clicar() {
     try {
-        let numero = Number(numero.value)
+        let numero = Number(num.value)
         if (numero < 0) {
             throw new Error('Esse número é incorreto')
         }
+        let raiz = Math.sqrt(numero)
+        resultado.innerHTML = `${raiz}`
     }catch (e) {
-        console.log(e)
+        window.alert(e)
+    }finally {
+        console.log('finally')
     }
 }
